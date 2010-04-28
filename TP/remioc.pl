7d03048700e46777e43dd73f17c6c087425d7d82
LUCASHPANDOLFO@gmail.com <LUCASHPANDOLFO@gmail.com@c2ff855d-f82c-6938-c156-421ca30fee5f>
2010-04-28 19:50:28 +0000

    --

--- TP/remioc.pl
+++ TP/remioc.pl
@@ -32,16 +32,11 @@ sub buscarRemitos{
 sub elegirRemitos(){
     #TODO: MENU
 
-    print "lista: @_\n";
-
-    print "CANTIDAD: $#_\n";
-
     return () if ($#_+1 == 0);
 
     my $FILAS;
     my %ARCHIVOS;
     foreach (@_){
-	print "linea: $_\n";
 	(my $CODIGO) = $_ =~ /(.*)\..+\.aproc$/;
 	$FILAS .= "$CODIGO $_ dummy ";
 	$ARCHIVOS{$CODIGO} = $_;
@@ -50,8 +45,6 @@ sub elegirRemitos(){
     my $ELEGIDOS=`dialog  --checklist \"Lista de remitos\"  24 50 12 $FILAS --stdout 2>/dev/null`;
 
     return () if $? != 0;
-
-    print "Elegidos: $ELEGIDOS\n";
 
     (my @TEMPORAL) = $ELEGIDOS =~ /"([^"]*)"/g;
     my @RELEGIDOS;
@@ -72,13 +65,11 @@ sub buscarUltimaOC(){
     #$_[0] -> ocgob o ocdet
     my $INICIAL=1;
     my $NUMERO;
-    opendir(DIR, "$grupo/oc/");
-    my @CANDIDATOS = grep(/$_[0]\..*/,readdir(DIR));
-    closedir(DIR);
+    my @CANDIDATOS = <$grupo/oc/$_[0].*>;
 
     foreach (@CANDIDATOS){
-	
-	($NUMERO) = $_ =~ /\.(.*)$/;
+
+	($NUMERO) = $_ =~ /\.([0-9]*)$/;
 	
 	if($NUMERO >= $INICIAL){
 	    $INICIAL=$NUMERO;
@@ -95,32 +86,27 @@ sub procesarOrden{
     my $NUMEROORDEN = shift(@_);
     my @REMITOS = @_;
 
-    print "Orden: $OCDET\n";
-    print "REMITOS: @REMITOS\n";
-
     # extraigo la información de todos los productos de los remitos
     my %PRODUCTOS = ();
     for my $origen (@REMITOS){
-	print "archivo $origen\n";
 	open(archivo, '<', "$grupo/aceptados/$origen") or die $!;
 	while(<archivo>){
 	    my $CODPROD = $_;
 	    ($CODPROD) = $CODPROD =~ /^[^;]*;([^;]*);/;
-	    print "linea: $_\n";
 	    my $CANTIDAD = $_;
 	    ($CANTIDAD) = $CANTIDAD =~ /^[^;]*;[^;]*;([^;]*);/;
 
 	    $PRODUCTOS{$CODPROD} += $CANTIDAD;
 	}
-	for my $clave (keys %PRODUCTOS){
-	    print "CODPROD: $clave -> $PRODUCTOS{$clave}\n";
-	}
 	
 	close archivo;
 	(my $destino) = $origen =~ /^(.*)\.aproc$/;
 	$destino .= ".proc";
-	print "renombro: aceptados/$origen a aceptados/$destino\n";
-	rename "$grupo/aceptados/$origen", "$grupo/aceptados/$destino";
+	`Glog $0 "Renombrando aceptados/$origen a aceptados/$destino" I`;
+	if (! rename "$grupo/aceptados/$origen", "$grupo/aceptados/$destino"){
+	    `Glog $0 "No se puede renombrar aceptados/$origen a aceptados/$destino" SE`;
+	    return -1;
+	}
     }
 
     #ahora, genero un nuevo OCDET y voy procesando los productos
@@ -135,9 +121,7 @@ sub procesarOrden{
     while(my $linea = <archivo>){
 	if( $linea =~ "^$NUMEROORDEN;"){
 	    #la linea es parte de la orden de compra que me interesa
-	    print "linea anterior: $linea\n";
 	    (my $CODPROD, my $REMANENTE) = $linea =~ "^[^;]*;[^;]*;([^;]*);([^;]*);";
-	    print "COD.PROD: $CODPROD, REMANENTE:$REMANENTE\n";
 
 	    my $ESTADO = "ABIERTO";
 	    if($REMANENTE <= $PRODUCTOS{$CODPROD}){
@@ -152,7 +136,6 @@ sub procesarOrden{
 
 	    $linea =~ s/^([^;]*;[^;]*;[^;]*);[^;]*;([^;]*);[^;]*;(.*)$/$1;$REMANENTE;$2;$ESTADO;$3/;
 
-	    print "linea modificada: $linea\n";
 	    print archivo2 $linea;
 	}
 	else{
@@ -165,7 +148,6 @@ sub procesarOrden{
     close archivo2;
 
     for my $clave (keys %PRODUCTOS){
-	print "CODPROD: $clave -> $PRODUCTOS{$clave}\n";
 	if($PRODUCTOS{$clave} > 0){
 	    print "Error: sobraron $PRODUCTOS{$clave} unidades del producto $clave\n";
 	    `Glog "$0" "Sobraron $PRODUCTOS{$clave} unidades del producto $clave, cuando se quería conciliar la orden de compra $NUMEROORDEN." "E"`;
@@ -199,10 +181,6 @@ elsif(length($PARAMETRO) == 8){
 
 }
 
-print "Orden de compra: $OC\n";
-print "Ultimo $ULTIMO\n";
-print "NUMERO: $NUMERO\n";
-
 #Si es orden de compra
 if( $OC ){
     #Verifico que este abierta
@@ -213,13 +191,28 @@ if( $OC ){
 
 	#Busco los remitos que se corresponden
 	my @RDISPONIBLES = &buscarRemitos("$NUMERO");
+	if( ! @RDISPONIBLES){
+	    print "No hay remitos disponibles para la orden de compra $NUMERO\n";
+	    `Glog "$0" "No hay remitos disponibles para la orden de compra $NUMERO." I`;
+	    exit 1;
+	}
 	
 	#Dejo al usuario elegir los remitos a procesar
 	my @RELEGIDOS = &elegirRemitos(@RDISPONIBLES);
+	
+	if( ! @RELEGIDOS){
+	    exit 2;
+	}
 
 	#Busco el ultimo archivo de descripción de ordenes de compra
 	$ULTIMO = &buscarUltimaOC("ocdet");
 	my $OCDET = "$grupo/oc/ocdet.$ULTIMO";
+	
+	if( ! $OCDET ){
+	    print "Error: No se encontro el archivo de descripción de ordenes de compra.";
+	    `Glog "$0" "No se encontro el archivo de descripción de ordenes de compra." SE`;
+	    exit 3;
+	}
 
 	#Procesa cada detalle de orden de compra utilizando los
 	#remitos elegidos
@@ -227,7 +220,8 @@ if( $OC ){
     }
     else{
 	#Esta cerrada
-	`Glog "$0" "Lo orden de compra $OC, no está abierta." W`;
+	print "Error: La orden de compra $OC, no está abierta.";
+	`Glog "$0" "La orden de compra $OC, no está abierta." E`;
     }
 }
 elsif($REMITO){
